40ad931dc95a306a15b0ae1858d0f70931d44b95
nursultanmaulen <maulen.n@list.ru>
2025-08-18 14:59:57 +0600

    aaaaaaaaa

--- frontend/front_visulex/src/App.js
+++ frontend/front_visulex/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn ABO
+          Learn Dasdasdasd
         </a>
       </header>
     </div>
